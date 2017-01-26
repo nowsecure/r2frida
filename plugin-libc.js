0a3f966b3ff19d0d23ee5664cd776e2479bd6ce2
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2017-01-26 15:02:06 +0100

    Initial implementation of the libc plugin

---
+++ plugin-libc.js
@@ -0,0 +1,41 @@
+// run '\. plugin.js' inside an 'r2 frida://' session to load it
+// run '\.-test' to unload it and '\.' to list hem all
+
+
+function sym(name, ret, arg) {
+  try {
+    return new NativeFunction(Module.findExportByName(null, name), ret, arg);
+  } catch (e) {
+    console.error(name, ':', e);
+  }
+}
+
+var libcCommands = {
+  'getuid': sym('getuid', 'int', []),
+  'getgid': sym('getgid', 'int', []),
+  'setuid': sym('setuid', 'int', ['int']),
+  'setgid': sym('setgid', 'int', ['int']),
+  'remove': sym('remove', 'int', ['string']),
+  'system': sym('system', 'int', ['string'])
+};
+
+r2frida.pluginRegister('libc', function(name) {
+  if (name === 'libc') {
+    return function(args) {
+      if (args.length === 0) {
+        return Object.keys(libcCommands).join('\n');
+      }
+      const command = args.shift();
+      for (var arg in args) {
+        if (+args[arg]) {
+          args[arg] = +arg[arg];
+        } else if (args[arg].substring(0, 2), '0x') {
+          args[arg] = ptr(arg[arg]);
+        } else if (args[arg] === 'true' || args[arg] === 'false') {
+          args[arg] = args[arg] === 'true';
+        }
+      }
+      return libcCommands[command].apply(null, args);
+    }
+  }
+});
