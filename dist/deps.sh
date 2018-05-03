aacf58a7756bb8f459d15ef0127245aa7a484a12
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2018-05-03 04:14:08 +0200

    Update debian package for node10 and r2-2.5.0

--- dist/deps.sh
+++ dist/deps.sh
@@ -1,14 +1,18 @@
-RV=1.7.0-git
+RV=2.5.0
 RA=amd64
-wget -c http://www.radare.org/get/debian/radare2_${RV}_${RA}.deb
-wget -c http://www.radare.org/get/debian/radare2-dev_${RV}_${RA}.deb
+wget -c http://radare.mikelloc.com/get/${RV}/radare2_${RV}_${RA}.deb
+wget -c http://radare.mikelloc.com/get/${RV}/radare2-dev_${RV}_${RA}.deb
+#wget -c http://www.radare.org/get/debian/radare2-dev_${RV}_${RA}.deb
 sudo dpkg -i radare2_${RV}_${RA}.deb
 sudo dpkg -i radare2-dev_${RV}_${RA}.deb
+
 # install NodeJS LTS
-NV=v6.11.2
-wget -c https://nodejs.org/dist/${NV}/node-${NV}-linux-x64.tar.xz
-tar xJvf node-${NV}-linux-x64.tar.xz
-export PATH=/tmp/node-${NV}:$PATH
+NV=v10.0.0
+NA=linux-x64
+wget -c https://nodejs.org/dist/${NV}/node-${NV}-${NA}.tar.xz
+cd /work
+tar xJf node-${NV}-${NA}.tar.xz -C /tmp
+export PATH=/tmp/node-${NV}-${NA}/bin:$PATH
 [ -z "${DESTDIR}" ] && DESTDIR=/
 [ -z "${R2_PLUGDIR}" ] && R2_PLUGDIR=/usr/lib/radare2/last/plugins
 make R2_PLUGDIR=${R2_PLUGDIR} DESTDIR=${DESTDIR}
