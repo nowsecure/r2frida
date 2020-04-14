0d94fb9bf8757bbad609cc1ea1d18acb63553946
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2020-04-14 20:40:42 +0200

    Add echo-log example script

---
+++ plugins/echo-log.js
@@ -0,0 +1,17 @@
+// run '\. echo-log.js' inside an 'r2 frida://' session to load it
+// run '\e file.log=/tmp/log.txt'
+// run '\echo hello world' 
+// run '\e-echo' 
+
+r2frida.pluginRegister('echo', function(name) {
+  if (name === 'echo') {
+    return function(args) {
+      if (args.length === 0) {
+        return 'Usage: \\echo message';
+      }
+      const message = args.join(' ');
+      r2frida.log(message);
+      return '';
+    }
+  }
+});
