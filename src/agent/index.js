332874b74c22a2dc0ade722e427f761e40159608
murphy <dark.cancerbero@gmail.com>
2021-01-09 19:43:16 +0100

    Add support to virtual paths in dlopen

--- src/agent/index.js
+++ src/agent/index.js
@@ -2286,7 +2286,16 @@ function getEnvJson () {
 }
 
 function dlopen (args) {
-  const path = args[0];
+  let path = args[0];
+  if (path.includes('\/r2f\/AppBundle')) {
+    const app_path = ObjC.classes.NSBundle.mainBundle().bundlePath();
+    path = path.replace('\/r2f\/AppBundle', app_path);  
+  } 
+  if (path.includes('\/r2f\/AppHome')) {
+    let NSHomeDirectory = new NativeFunction(Module.findExportByName(null, 'NSHomeDirectory'), 'pointer', []);
+    const appHome = new ObjC.Object(NSHomeDirectory()).toString()
+    path = path.replace('\/r2f\/AppHome', appHome);  
+  } 
   return Module.load(path);
 }
 
