c5fced683979790632f775e21ce041d9166d66be
Apkunpacker <27184655+apkunpacker@users.noreply.github.com>
2021-05-20 16:27:41 +0530

    Add files via upload

---
+++ plugins/inject-getpid.js
@@ -0,0 +1,17 @@
+"use strict";
+if (Process.arch == "arm64" && Process.platform == "linux") {
+    var getBaseCode = Memory.alloc(Process.pageSize);
+    Memory.patchCode(getBaseCode, Process.pageSize, function(code) {
+        var arm64Writer = new Arm64Writer(code, {
+            pc: getBaseCode
+        });
+        arm64Writer.putInstruction(0xd2801588);
+        arm64Writer.putInstruction(0xd2800020);
+        arm64Writer.putInstruction(0xd4000001);
+        arm64Writer.putRet();
+        arm64Writer.flush();
+    });
+    const fun = new NativeFunction(getBaseCode, 'int', []);
+    console.log("getPid From syscall : ", fun());
+} else
+    console.log("Unsupported arch");
