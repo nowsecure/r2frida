76fbc4d7fd6d32d71859b96955b9cb6ef399cbc5
pancake <pancake@nopcode.org>
2021-08-19 01:49:34 +0200

    Extend the gnuboy plugin

--- plugins/gnuboy.js
+++ plugins/gnuboy.js
@@ -12,8 +12,19 @@ function sym(name, ret, arg) {
 
 function showHelp() {
   return 'gnuboy Commands:\n'
+  + '# This plugin requires to be attached to "r2pm -ci gnuboy"\n'
   + 'gb io    - make r2frida read/write gameboy memory\n'
   + 'gb noio  - disable that hook and go back to process io\n'
+  + '# CPU\n'
+  + 'gb reset - clear screen\n'
+  + 'gb cpu   - show contents of cpu struct\n'
+  + 'gb stop  - stop execution\n'
+  + 'gb step  - perform one step\n'
+  + 'gb cont  - continue execution\n'
+  + '# APU\n'
+  + 'gb mute  - mute audio\n'
+  + '# LCD\n'
+  + 'gb cls   - clear screen\n'
 }
 
 const gnuboyRead = sym('mem_read', 'int', ['int']);
@@ -21,7 +32,7 @@ const gnuboyWrite = sym('mem_write', 'void', ['int', 'uint8']);
 
 function hookedRead (offset, count) {
   var i = 0;
-  var data = new Buffer(count);
+  var data = []; // new ArrayBuffer(count);
   for (i = 0; i < count; i++) {
     data[i] = gnuboyRead(offset + i);
   }
@@ -30,12 +41,79 @@ function hookedRead (offset, count) {
 
 function hookedWrite (offset, data) {
   var i = 0;
-  var b = new Buffer(data);
+  var b = []; // new ArrayBuffer(data);
   for (i = 0; i < b.length; i++) {
     gnuboyWrite(offset + i, b[i]);
   }
   return [{}, null];
 };
+
+function showCpu() {
+  const cpu_addr = Module.findExportByName(null, 'cpu');
+  let r = '?e f cpu = ' + cpu_addr + '\n';
+  r += 's '+cpu_addr+';pf wwwwwwxxxxxxxx pc sp bc de hl af ime ima speed halt div tim lcdc snd;s--\n'
+  return r;
+}
+
+var cpu_emulate_hook = null;
+var cpu_emulate = sym('cpu_emulate', 'void', ['int']);
+var cpu_emulate_orig = cpu_emulate;
+var usleep = sym('usleep', 'void', ['int']);
+
+
+function cpuStop() {
+  if (cpu_emulate_hook) {
+    return;
+  }
+  const cb = new NativeCallback(function (cycles){}, 'void', ['int']);
+  Interceptor.replace(cpu_emulate, cb);
+  cpu_emulate_hook = true;
+}
+
+function cpuStep() {
+  if (cpu_emulate_hook) {
+    Interceptor.revert(cpu_emulate);
+  Interceptor.replace(cpu_emulate, cpu_emulate_orig);
+    Interceptor.flush();
+    cpu_emulate_hook = false;
+  }
+  cpu_emulate(1);
+  cpuStop();
+}
+
+function cpuCont() {
+  if (cpu_emulate_hook) {
+    Interceptor.revert(cpu_emulate);
+    Interceptor.flush();
+    cpu_emulate_hook = false;
+  }
+}
+
+function soundOff() {
+  const sound_reset = sym('sound_reset', 'void', []);
+  if (sound_reset) {
+    sound_reset();
+  }
+  const sound_write = sym('sound_write', 'void', []);
+  const cb = new NativeCallback(function (cycles){}, 'void', ['int']);
+  Interceptor.replace(sound_write, cb);
+}
+
+function cpuReset() {
+  const cpu_reset = sym('cpu_reset', 'void', []);
+  if (cpu_reset) {
+    cpu_reset();
+  }
+}
+
+function clearScreen() {
+  const clear_screen = sym('lcd_reset', 'void', []);
+  if (cpu_reset) {
+    cpu_reset();
+  }
+}
+
+r2frida.pluginUnregister('gb');
 
 r2frida.pluginRegister('gb', function(name) {
   if (name === 'gb') {
@@ -45,6 +123,27 @@ r2frida.pluginRegister('gb', function(name) {
       }
       const command = args.shift();
       switch(command) {
+      case 'reset':
+        cpuReset();
+        break;
+      case 'stop':
+        cpuStop();
+        break;
+      case 'step':
+        cpuStep();
+        break;
+      case 'mute':
+        soundOff();
+        break;
+      case 'cont':
+        cpuCont();
+        break;
+      case 'cls':
+        clearScreen();
+        break;
+      case 'cpu':
+        return showCpu();
+        break;
       case 'io':
         r2frida.hookedRead = hookedRead;
         r2frida.hookedWrite = hookedWrite;
