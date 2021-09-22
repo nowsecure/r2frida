00e84d298c382189caceac0573f5419330ccdc93
unabomber78 <77119441+unabomber78@users.noreply.github.com>
2021-09-22 17:27:34 +0700

    Update io_frida.c

--- src/io_frida.c
+++ src/io_frida.c
@@ -802,7 +802,7 @@ static char *__system_continuation(RIO *io, RIODesc *fd, const char *command) {
 		io->cb_printf (" p/x = show pointer in hexadecimal\n");
 		io->cb_printf ("  c  = show value as a string (char)\n");
 		io->cb_printf ("  i  = show decimal argument\n");
-		io->cb_printf ("  z  = show pointer to UTF-8 string\n");
+		io->cb_printf ("  z  = show pointer to string\n");
 		io->cb_printf ("  w  = show pointer to UTF-16 string\n");
 		io->cb_printf ("  a  = show pointer to ANSI string\n");
 		io->cb_printf ("  h  = hexdump from pointer (optional length, h16 to dump 16 bytes)\n");
