95cf0392de2816c0eb0448a35a403650ee97be21
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-10-26 12:17:26 +0200

    Handle errors when decoding base64

--- src/agent/fs.js
+++ src/agent/fs.js
@@ -14,7 +14,12 @@ module.exports = {
 
 function debase(a) {
   if (a.startsWith('base64:')) {
-    return normalize(toByteArray(a.slice(7)));
+    try {
+      const data = toByteArray(a.slice(7)));
+      a = String.fromCharCode.apply(null, data);
+    } catch(e) {
+      // invalid base64
+    }
   }
   return normalize(a);
 }
