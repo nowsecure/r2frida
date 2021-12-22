dde0af000951cc51f993a84f17d159bb3e375a43
Murphy <dark.cancerbero@gmail.com>
2021-12-22 21:42:49 +0100

    Add invalid chars in sanitizeString
    * Remove flagify and unify badchars into sanitizeString

--- src/agent/utils.js
+++ src/agent/utils.js
@@ -1,13 +1,8 @@
 module.exports = {
-  flagify,
   sanitizeString
 };
 
 function sanitizeString (str) {
-  const specialChars = '`${}~|*;#@&<> ()';
+  const specialChars = '/\\`+-${}~|*,;:\"\'#@&<> ()[]!?%';
   return str.split('').map(c => specialChars.indexOf(c) === -1 ? c : '_').join('');
-}
-
-function flagify (s) {
-  return s.replace(/[\s-\/\\()\[\]<>!?$;%\*@`|&"+,]/g, '_');
 }
