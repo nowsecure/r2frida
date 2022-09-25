9e9355b27d88b1c3da8f9624edd40f12c913a4f4
Murphy <me@0xmurphy.me>
2022-09-25 13:30:09 +0200

    Migrate trace cmds to trace module

--- src/agent/java/index.js
+++ src/agent/java/index.js
@@ -140,6 +140,38 @@ function javaPerform (fn) {
   return Java.performNow(fn);
 }
 
+function traceJava (klass, method) {
+  javaPerform(function () {
+    const Throwable = Java.use('java.lang.Throwable');
+    const k = javaUse(klass);
+    k[method].implementation = function (args) {
+      const res = this[method]();
+      const bt = config.getBoolean('hook.backtrace')
+        ? Throwable.$new().getStackTrace().map(_ => _.toString())
+        : [];
+      const traceMessage = {
+        source: 'dt',
+        klass: klass,
+        method: method,
+        backtrace: bt,
+        timestamp: new Date(),
+        result: res,
+        values: args
+      };
+      if (config.getString('hook.output') === 'json') {
+        log.traceEmit(traceMessage);
+      } else {
+        let msg = `[JAVA TRACE][${traceMessage.timestamp}] ${klass}:${method} - args: ${JSON.stringify(args)}. Return value: ${res.toString()}`;
+        if (config.getBoolean('hook.backtrace')) {
+          msg += ` backtrace: \n${traceMessage.backtrace.toString().split(',').join('\nat ')}\n`;
+        }
+        log.traceEmit(msg);
+      }
+      return res;
+    };
+  });
+}
+
 module.exports = {
   JavaAvailable,
   javaUse,
@@ -147,5 +179,11 @@ module.exports = {
   performOnJavaVM,
   waitForJava,
   listJavaClassesJson,
+<<<<<<< HEAD
   javaPerform
+=======
+  listJavaClassesJsonSync,
+  javaPerform,
+  traceJava
+>>>>>>> 6c58d36 (Migrate trace cmds to trace module)
 };
