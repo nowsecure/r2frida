a42e242d393e119a4fa736311eb905e22c5e0c0a
Murphy <me@0xmurphy.me>
2022-09-25 13:29:51 +0200

    add traceSwift to swift module

--- src/agent/darwin/swift.js
+++ src/agent/darwin/swift.js
@@ -1,11 +1,31 @@
 'use strict';
 
 const config = require('./config');
+const log = require('./log');
+const utils = require('./utils');
 
 const SwiftAvailable = function () {
   return config.getBoolean('want.swift') && Process.platform === 'darwin' && global.hasOwnProperty('Swift') && Swift.available;
 };
 
+function traceSwift (klass, method) {
+  if (!SwiftAvailable()) {
+    return;
+  }
+  const targetAddress = utils.getPtr('swift:' + klass + '.' + method);
+  if (ptr(0).equals(targetAddress)) {
+    console.error('Missing method ' + method + ' in class ' + klass);
+    return;
+  }
+
+  const callback = function (args) {
+    const msg = ['[SWIFT]', klass, method, JSON.stringify(args)];
+    log.traceEmit(msg.join(' '));
+  };
+  Swift.Interceptor.Attach(targetAddress, callback);
+}
+
 module.exports = {
-  SwiftAvailable
+  SwiftAvailable,
+  traceSwift
 };
