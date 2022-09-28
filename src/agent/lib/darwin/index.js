955c37735b00dc918b9e025f776d3fef548236d1
Murphy <me@0xmurphy.me>
2022-09-28 12:48:41 +0200

    Migrate r2 cmds to r2 module

--- src/agent/lib/darwin/index.js
+++ src/agent/lib/darwin/index.js
@@ -12,6 +12,15 @@ const ISA_MAGIC_VALUE = ptr('0x000001a000000001');
 /* ObjC.available is buggy on non-objc apps, so override this */
 const ObjCAvailable = (Process.platform === 'darwin') && !(Java && Java.available) && ObjC && ObjC.available && ObjC.classes && typeof ObjC.classes.NSString !== 'undefined';
 
+function initFoundation () {
+  // required for early instrumentation
+  try {
+    Module.load('/System/Library/Frameworks/Foundation.framework/Foundation');
+  } catch (e) {
+    // ignored
+  }
+}
+
 function getIOSVersion () {
   const processInfo = ObjC.classes.NSProcessInfo.processInfo();
   const versionString = processInfo.operatingSystemVersionString().UTF8String().toString();
@@ -371,6 +380,7 @@ class IOSPathTransform extends PathTransform {
 }
 
 module.exports = {
+  initFoundation,
   getIOSVersion,
   isiOS,
   isObjC,
--- src/agent/lib/r2.js
+++ src/agent/lib/r2.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const globals = require('./globals');
+const { sym } = require('./sys');
 const utils = require('./utils');
 
 let cmdSerial = 0;
+
+// r2->io->frida->r2pipe->r2
+let _r2 = null;
+let _r_core_new = null; // eslint-disable-line camelcase
+let _r_core_cmd_str = null; // eslint-disable-line camelcase
+let _r_core_free = null; // eslint-disable-line camelcase,no-unused-vars
+let _free = null;
 
 function getR2Arch (arch) {
   switch (arch) {
@@ -88,10 +96,61 @@ function _sendCommand (cmd, serial) {
   }
 }
 
+function radareSeek (args) {
+  const addr = utils.getPtr('' + args);
+  const cmdstr = 's ' + (addr || '' + args);
+  return cmdstr;
+  // XXX hangs
+  // return r2.hostCmd(cmdstr);
+}
+
+function radareCommand (args) {
+  const cmd = args.join(' ');
+  if (cmd.length === 0) {
+    return 'Usage: :r [cmd]';
+  }
+  if (_radareCommandInit()) {
+    return _radareCommandString(cmd);
+  }
+  return ':dl /tmp/libr.dylib';
+}
+
+function _radareCommandInit () {
+  if (_r2) {
+    return true;
+  }
+  if (!_r_core_new) {
+    _r_core_new = sym('r_core_new', 'pointer', []);
+    if (!_r_core_new) {
+      console.error('ERROR: Cannot find r_core_new. Do :dl /tmp/libr.dylib');
+      return false;
+    }
+    _r_core_cmd_str = sym('r_core_cmd_str', 'pointer', ['pointer', 'pointer']);
+    _r_core_free = sym('r_core_free', 'void', ['pointer']);
+    _free = sym('free', 'void', ['pointer']);
+    _r2 = _r_core_new();
+  }
+  return true;
+}
+
+function _radareCommandString (cmd) {
+  if (_r2) {
+    const aCmd = Memory.allocUtf8String(cmd);
+    const ptr = _r_core_cmd_str(_r2, aCmd);
+    const str = Memory.readCString(ptr);
+    _free(ptr);
+    return str;
+  }
+  console.error('Warning: not calling back r2');
+  return '';
+}
+
 module.exports = {
   getR2Arch,
   hostCmds,
   hostCmd,
   hostCmdj,
-  onCmdResp
+  onCmdResp,
+  radareSeek,
+  radareCommand
 };
