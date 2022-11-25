86caa62bf5144f09f7ead8aef17b52b7aacab223
Murphy <me@0xmurphy.me>
2022-11-25 18:31:49 +0100

    Reindent anal, disasm, dump, expr

--- src/agent/lib/anal.js
+++ src/agent/lib/anal.js
@@ -1,42 +1,43 @@
-import classes from "./info/classes.js";
-import darwin from "./darwin/index.js";
+import classes from './info/classes.js';
+import darwin from './darwin/index.js';
 'use strict';
-function analFunctionSignature(args) {
-    if (!darwin.ObjCAvailable) {
-        return 'Error: afs is only implemented for ObjC methods.';
+
+function analFunctionSignature (args) {
+  if (!darwin.ObjCAvailable) {
+    return 'Error: afs is only implemented for ObjC methods.';
+  }
+  if (args.length === 0) {
+    return 'Usage: afs [class] [method]';
+  }
+  if (args.length === 1) {
+    return classes.listClasses(args);
+  }
+  if (args.length > 1) {
+    const klassName = args[0];
+    const methodName = args[1].replace(/:/g, '_');
+    const klass = ObjC.classes[klassName];
+    if (!klass) {
+      // try to resolve from DebugSymbol
+      const at = klassName.startsWith('0x') ? DebugSymbol.fromAddress(ptr(klassName)) : DebugSymbol.fromName(klassName);
+      if (at) {
+        return JSON.stringify(at);
+      }
+      return 'Cannot find class named ' + klassName;
     }
-    if (args.length === 0) {
-        return 'Usage: afs [class] [method]';
+    // const instance = ObjC.chooseSync(ObjC.classes[klassName])[0];
+    const instance = ObjC.chooseSync(klass)[0];
+    if (!instance) {
+      return 'Cannot find any instance for ' + klassName;
     }
-    if (args.length === 1) {
-        return classes.listClasses(args);
+    const method = instance[methodName];
+    if (!method) {
+      return 'Cannot find method ' + methodName + ' for class ' + klassName;
     }
-    if (args.length > 1) {
-        const klassName = args[0];
-        const methodName = args[1].replace(/:/g, '_');
-        const klass = ObjC.classes[klassName];
-        if (!klass) {
-            // try to resolve from DebugSymbol
-            const at = klassName.startsWith('0x') ? DebugSymbol.fromAddress(ptr(klassName)) : DebugSymbol.fromName(klassName);
-            if (at) {
-                return JSON.stringify(at);
-            }
-            return 'Cannot find class named ' + klassName;
-        }
-        // const instance = ObjC.chooseSync(ObjC.classes[klassName])[0];
-        const instance = ObjC.chooseSync(klass)[0];
-        if (!instance) {
-            return 'Cannot find any instance for ' + klassName;
-        }
-        const method = instance[methodName];
-        if (!method) {
-            return 'Cannot find method ' + methodName + ' for class ' + klassName;
-        }
-        return method.returnType + ' (' + method.argumentTypes.join(', ') + ');';
-    }
-    return 'Usage: afs [klassName] [methodName]';
+    return method.returnType + ' (' + method.argumentTypes.join(', ') + ');';
+  }
+  return 'Usage: afs [klassName] [methodName]';
 }
 export { analFunctionSignature };
 export default {
-    analFunctionSignature
+  analFunctionSignature
 };
--- src/agent/lib/expr.js
+++ src/agent/lib/expr.js
@@ -1,31 +1,35 @@
-import r2 from "./r2.js";
+import r2 from './r2.js';
 'use strict';
-function numEval(expr) {
-    return new Promise((resolve, reject) => {
-        const symbol = DebugSymbol.fromName(expr);
-        if (symbol && symbol.name) {
-            return resolve(symbol.address);
-        }
-        r2.hostCmd('?v ' + expr).then(_ => resolve(_.trim())).catch(reject);
+
+function numEval (expr) {
+  return new Promise((resolve, reject) => {
+    const symbol = DebugSymbol.fromName(expr);
+    if (symbol && symbol.name) {
+      return resolve(symbol.address);
+    }
+    r2.hostCmd('?v ' + expr).then(_ => resolve(_.trim())).catch(reject);
+  });
+}
+
+function evalNum (args) {
+  return new Promise((resolve, reject) => {
+    numEval(args.join(' ')).then(res => {
+      resolve(res);
     });
+  });
 }
-function evalNum(args) {
-    return new Promise((resolve, reject) => {
-        numEval(args.join(' ')).then(res => {
-            resolve(res);
-        });
-    });
+
+function evalCode (args) {
+  const code = args.join(' ');
+  const result = eval(code); // eslint-disable-line
+  return (result !== undefined) ? result : '';
 }
-function evalCode(args) {
-    const code = args.join(' ');
-    const result = eval(code); // eslint-disable-line
-    return (result !== undefined) ? result : '';
-}
+
 export { numEval };
 export { evalNum };
 export { evalCode };
 export default {
-    numEval,
-    evalNum,
-    evalCode
+  numEval,
+  evalNum,
+  evalCode
 };
