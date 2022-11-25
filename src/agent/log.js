1207ed256e2446e85a3fbd456e5e8ba4e86f89d2
Murphy <me@0xmurphy.me>
2022-11-25 19:24:49 +0100

    Reindent rest of files

--- src/agent/log.js
+++ src/agent/log.js
@@ -1,32 +1,35 @@
-import config from "./config.js";
-import { wrapStanza } from "./lib/utils.js";
+import config from './config.js';
+import { wrapStanza } from './lib/utils.js';
 'use strict';
+
 export const logs = [];
-export function traceEmit(msg) {
-    const fileLog = config.getString('file.log');
-    if (fileLog.length > 0) {
-        send(wrapStanza('log-file', {
-            filename: fileLog,
-            message: msg
-        }));
-    }
-    else {
-        traceLog(msg);
-    }
-    if (config.getBoolean('hook.logs')) {
-        logs.push(msg);
-    }
-    global.r2frida.logs = logs;
+
+export function traceEmit (msg) {
+  const fileLog = config.getString('file.log');
+  if (fileLog.length > 0) {
+    send(wrapStanza('log-file', {
+      filename: fileLog,
+      message: msg
+    }));
+  } else {
+    traceLog(msg);
+  }
+  if (config.getBoolean('hook.logs')) {
+    logs.push(msg);
+  }
+  global.r2frida.logs = logs;
 }
-export function traceLog(msg) {
-    if (config.getBoolean('hook.verbose')) {
-        send(wrapStanza('log', {
-            message: msg
-        }));
-    }
+
+export function traceLog (msg) {
+  if (config.getBoolean('hook.verbose')) {
+    send(wrapStanza('log', {
+      message: msg
+    }));
+  }
 }
+
 export default {
-    logs,
-    traceEmit,
-    traceLog
+  logs,
+  traceEmit,
+  traceLog
 };
--- src/agent/plugin.js
+++ src/agent/plugin.js
@@ -1,47 +1,51 @@
 const commandHandlers = {};
-function pluginRegister(name, ch) {
-    if (name in commandHandlers) {
-        console.log('Cannot register the same handler twice');
-        return false;
-    }
-    commandHandlers[name] = ch;
-    return true;
-}
-function pluginUnregister(name) {
-    if (name in commandHandlers) {
-        delete commandHandlers[name];
-        return true;
-    }
+
+function pluginRegister (name, ch) {
+  if (name in commandHandlers) {
+    console.log('Cannot register the same handler twice');
     return false;
+  }
+  commandHandlers[name] = ch;
+  return true;
 }
-function commandHandler(name) {
-    for (const key of Object.keys(commandHandlers)) {
-        const ch = commandHandlers[key];
-        if (typeof ch === 'function') {
-            const handler = ch(name);
-            if (handler !== undefined) {
-                return handler;
-            }
-        }
+
+function pluginUnregister (name) {
+  if (name in commandHandlers) {
+    delete commandHandlers[name];
+    return true;
+  }
+  return false;
+}
+
+function commandHandler (name) {
+  for (const key of Object.keys(commandHandlers)) {
+    const ch = commandHandlers[key];
+    if (typeof ch === 'function') {
+      const handler = ch(name);
+      if (handler !== undefined) {
+        return handler;
+      }
     }
-    return undefined;
+  }
+  return undefined;
 }
-function pluginList() {
-    return Object.keys(commandHandlers).join('\n');
+
+function pluginList () {
+  return Object.keys(commandHandlers).join('\n');
 }
 
 // import packageJson from "./package.json" assert { type: "json" };
 
 global.r2frida = {
-    version: "5.7.8", // packageJson.version,
-    commandHandler: commandHandler,
-    pluginRegister: pluginRegister,
-    pluginUnregister: pluginUnregister,
-    pluginList: pluginList,
-    // io hook plugin API //
-    hookedRead: null,
-    hookedWrite: null,
-    hookedRanges: null,
-    hookedScan: null
+  version: "5.7.8", // packageJson.version,
+  commandHandler: commandHandler,
+  pluginRegister: pluginRegister,
+  pluginUnregister: pluginUnregister,
+  pluginList: pluginList,
+  // io hook plugin API //
+  hookedRead: null,
+  hookedWrite: null,
+  hookedRanges: null,
+  hookedScan: null
 };
 export default global.r2frida;
