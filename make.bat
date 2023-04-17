28f059d3d6ec50c66082718dce27824edab2509a
Eduardo Novella <foro.dudu@gmail.com>
2023-04-17 18:05:09 +0200

    Update to Frida 16.0.13

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-set frida_version=16.0.12
+set frida_version=16.0.13
 if "%PLATFORM%" == "x64" (set frida_os_arch=x86_64) else (set frida_os_arch=x86)
 set DEBUG=/O2
 
