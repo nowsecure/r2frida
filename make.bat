ce856d82eb2bed7420187ea75918ac00c1ee6c8c
pancake <pancake@nopcode.org>
2023-04-19 11:13:39 +0200

    Update to Frida 16.0.15

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-set frida_version=16.0.14
+set frida_version=16.0.15
 if "%PLATFORM%" == "x64" (set frida_os_arch=x86_64) else (set frida_os_arch=x86)
 set DEBUG=/O2
 
