7d2262acbcd9843942e280a3ce180b245489b50e
Roshanlal <roshanlaladchitre103@gmail.com>
2024-04-16 22:12:54 +0530

    Select windows build arch target
    Made further refinements. Now user can select arm and arm64 as target system

--- preconfigure.bat
+++ preconfigure.bat
@@ -1,5 +1,7 @@
 :: This is a comment
 @echo off
+SETLOCAL EnableDelayedExpansion
+
 :: Preconfigure script for Windows
 
 echo === Finding Git...
@@ -12,25 +14,56 @@ if %ERRORLEVEL% == 0 (
 )
 git pull
 
-set PLATFORM=x64
-
 REM VisualStudio uses HOST_TARGET syntax, so: x86_amd64 means 32bit compiler for 64bit target
 REM: Hosts: x86 amd64 x64
 REM: Targets: x86 amd64 x64 arm arm64
+REM Detect the host architecture intuitively and easily
 
-:: Set VSARCH based on command line input or system architecture
-if "%*" == "x86" (
-  set VSARCH=x86
-) else if "%*" == "x64" (
-  set VSARCH=x64
-) else (
-  :: Automatically determine the architecture if no argument is provided
-  if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-    set VSARCH=x64
-  ) else (
-    set VSARCH=x86
-  )
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    SET "HOST_ARCH=amd64"
+) ELSE IF "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    SET "HOST_ARCH=x86"
+) ELSE (
+    SET "HOST_ARCH=unknown"
 )
+
+REM Check if arguments are passed
+IF "%~1"=="" (
+    echo Your current Host Architecture is !HOST_ARCH!
+    ECHO Please select the Target Architecture:
+    ECHO 1. x86
+    ECHO 2. amd64 [x64]
+    ECHO 3. arm
+    ECHO 4. arm64
+    SET /P "CHOICE=Enter your choice (1-4): "
+
+    REM Set target architecture based on user input
+    IF "!CHOICE!"=="1" (
+        SET "TARGET_ARCH=x86"
+    ) ELSE IF "!CHOICE!"=="2" (
+        SET "TARGET_ARCH=amd64"
+    ) ELSE IF "!CHOICE!"=="3" (
+        SET "TARGET_ARCH=arm"
+    ) ELSE IF "!CHOICE!"=="4" (
+        SET "TARGET_ARCH=arm64"
+    ) ELSE (
+        ECHO Invalid choice. Defaulting to arm64.
+        SET "TARGET_ARCH=arm64"
+    )
+
+    REM Check if target and host are the same and set VSARCH accordingly
+    IF "!TARGET_ARCH!"=="!HOST_ARCH!" (
+        SET "VSARCH=!HOST_ARCH!"
+    ) ELSE (
+        SET "VSARCH=!HOST_ARCH!_!TARGET_ARCH!"
+    )
+
+) ELSE (
+    REM Use provided host_target argument
+    SET "VSARCH=%1"
+)
+
+ECHO VSARCH is set to: !VSARCH!
 
 echo === Finding Visual Studio...
 cl --help > NUL 2> NUL
@@ -77,3 +110,4 @@ if %ERRORLEVEL% == 0 (
 )
 
 echo Now you can run 'make.bat'
+ENDLOCAL
