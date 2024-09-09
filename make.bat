42512dba21f510c0f017847957df354951afc918
pancake <pancake@nopcode.org>
2024-09-09 17:36:05 +0200

    ji

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 REM setlocal EnableDelayedExpansion
-set frida_version=16.5.0
+set frida_version=16.5.1
 set r2frida_version=5.9.4
 if "%PLATFORM%" == "x64" (set frida_os_arch=x86_64) else (set frida_os_arch=x86)
 set DEBUG=/O2
