1d0eedd03a8dfc80f341105f8bf2773af5fffc04
theShinigami <salimabdellanuru@gmail.com>
2025-06-19 20:34:23 +0300

    feat: add function to execute and display system commands

--- src/agent/lib/debug/system.ts
+++ src/agent/lib/debug/system.ts
@@ -23,3 +23,45 @@ export function runSystem(command: string): string | null {
 export function runSystemCommand(args: string[]): string | null {
     return runSystem(args.join(" "));
 }
+
+
+export function runCommandAsString(command: string): string | null {
+
+    const popenSymbol = getGlobalExportByName("popen");
+    const fgetsSymbol = getGlobalExportByName("fgets");
+    const pcloseSymbol = getGlobalExportByName("pclose");
+
+    if (popenSymbol !== null && fgetsSymbol !== null && pcloseSymbol !== null) {
+        const popen = new NativeFunction(popenSymbol, "pointer", ["pointer", "pointer"]);
+        const fgets = new NativeFunction(fgetsSymbol, "pointer", ["pointer", "int", "pointer"]);
+        const pclose = new NativeFunction(pcloseSymbol, "int", ["pointer"]);
+
+        const libcCommand = Memory.allocUtf8String(command);
+        const libcCommandMode = Memory.allocUtf8String("r");
+
+        const file = popen(libcCommand, libcCommandMode);
+        if (file.isNull()) {
+            return null;
+        }
+
+        const bufferSize: number = 1024;
+        const buffer = Memory.alloc(bufferSize);
+        let output = "";
+        while (!(fgets(buffer, bufferSize, file)).isNull()) {
+            output += buffer.readUtf8String();
+        }
+        pclose(file);
+
+        return output;
+
+    }
+
+    return null;
+
+}
+
+
+export function runSystemCommandAsString(args: string[]): string | null {
+    return runCommandAsString(args.join(" "));
+}
+
