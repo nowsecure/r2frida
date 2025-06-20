9006f718779401deefc66b661be35b34bf330e69
pancake <pancake@nopcode.org>
2025-06-20 13:17:52 +0200

    Introduce the new ABI version field required since r2-5.9.9

--- src/io_frida.c
+++ src/io_frida.c
@@ -2160,6 +2160,9 @@ R_API RLibStruct radare_plugin = {
 	.type = R_LIB_TYPE_IO,
 	.data = &r_io_plugin_frida,
 	.version = R2_VERSION,
+#if R2_VERSION_NUMBER >= 50809
+	.abi_version = R_LIB_CURRENT_ABI_VERSION,
+#endif
 #if ((R2_VERSION_MAJOR == 4 && R2_VERSION_MINOR >= 2) || R2_VERSION_MAJOR > 4)
 	.pkgname = "r2frida"
 #endif
