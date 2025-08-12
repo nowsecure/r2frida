4b51db5a24693f813fb08528114f3ea69ad80621
pancake <pancake@nopcode.org>
2025-08-12 19:19:30 +0200

    Update for Frida 17.2.15

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 REM setlocal EnableDelayedExpansion
-set frida_version=17.2.14
+set frida_version=17.2.15
 set r2frida_version=6.0.0
 if "%PLATFORM%" == "x64" (set frida_os_arch=x86_64) else (set frida_os_arch=x86)
 set DEBUG=/O2
